eeb70e2ae3cf2f6cbaf64fec686126a5f0469c36
damiecode <codenlyn@gmail.com>
2020-08-09 13:13:00 +0100

    Add 404 error page

--- src/components/NotFoundPage.js
+++ src/components/NotFoundPage.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import PageNotFound from '../404.jpg';
 
 const NotFoundPage = () => (
   <div>
-    404 - <Link to="/">Go home</Link>
+    <img src={PageNotFound} alt="Page Not Found" />
+    <p style={{ textAlign: 'center' }}>
+      <Link to="/">Go to Home </Link>
+    </p>
   </div>
 );
 
