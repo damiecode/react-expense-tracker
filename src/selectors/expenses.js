f815443f13fa1a3770879cde704c227a712528e9
damiecode <codenlyn@gmail.com>
2020-08-20 10:02:13 +0100

    Remove description from expenses

--- src/selectors/expenses.js
+++ src/selectors/expenses.js
@@ -1,14 +1,13 @@
 import moment from 'moment';
 
 export default (expenses, {
-  text, sortBy, startDate, endDate,
+  sortBy, startDate, endDate,
 }) => expenses.filter(expense => {
   const createdAtMoment = moment(expense.createdAt);
   const startDateMatch = startDate ? startDate.isSameOrBefore(createdAtMoment, 'day') : true;
   const endDateMatch = endDate ? endDate.isSameOrAfter(createdAtMoment, 'day') : true;
-  const textMatch = expense.description.toLowerCase().includes(text.toLowerCase());
 
-  return startDateMatch && endDateMatch && textMatch;
+  return startDateMatch && endDateMatch;
 }).sort((a, b) => {
   if (sortBy === 'date') {
     return a.createdAt < b.createdAt ? 1 : -1;
